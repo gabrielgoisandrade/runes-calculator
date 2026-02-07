efd4258c136db9633ccccd233db2f279829eef4d
Gabriel Gois Andrade <gabriel.gois.andrade14@gmail.com>
2026-02-07 00:28:17 -0300

    chore: ajuste no page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,13 @@
 import { Form } from './Form'
 import styles from './page.module.scss'
-
-
+import { Tutorial } from './Tutorial/index'
 
 export default function Home() {
     return (
         <div className={styles.page}>
             <Form />
+
+            <Tutorial />
         </div>
     )
 }
